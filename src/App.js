b7330c48187f88641a1aebe75c8cc94d6f9f2146
Tahsina Laboni <tahsinaa404@gmail.com>
2022-03-16 12:31:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import Home from "./component/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route,Link  } from "react-router-dom";
+
+import Services from "./component/Services/Services";
+import About from "./component/About/About";
+import Contacts from "./component/Contacts/Contacts";
+import NotFound from "./component/NotFound/NotFound";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        {/* Here Im a useing Reate Router */}
+<Router>
+      <Switch>
+        <Route exact path="/">   
+        
+         <Home></Home>
+        </Route>
+      <Route path="/home">
+         <Home></Home>
+      </Route>
+      <Route path="/service">
+        <Services></Services>
+      </Route>
+      <Route path="/about">
+        <About></About>
+      </Route>
+      <Route path="/contact">
+        <Contacts></Contacts>
+      </Route>
+      <Route path="*">
+        <NotFound></NotFound>
+      </Route>
+
+      </Switch>
+</Router>
+
+
+
+    </div>
+  );
+}
+
+export default App;
